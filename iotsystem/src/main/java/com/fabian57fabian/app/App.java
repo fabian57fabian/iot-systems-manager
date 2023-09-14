048b5e416446e02c8b86fbabb385b2a1ada2c744
fabian57fabian <fabiangreavu@gmail.com>
2023-09-14 23:47:59 +0200

    Created MAven project

---
+++ iotsystem/src/main/java/com/fabian57fabian/app/App.java
@@ -0,0 +1,13 @@
+package com.fabian57fabian.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
