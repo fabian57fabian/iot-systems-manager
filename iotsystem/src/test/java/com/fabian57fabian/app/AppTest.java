2bf565484b46d433057da674928ecb701abd6a73
fabian57fabian <fabiangreavu@gmail.com>
2023-09-18 18:44:54 +0200

    Added app empty test for coverage

---
+++ iotsystem/src/test/java/com/fabian57fabian/app/AppTest.java
@@ -0,0 +1,21 @@
+package com.fabian57fabian.app;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class AppTest {
+	App app;
+
+	@Before
+	public void setUp() throws Exception {
+		app = new App();
+	}
+	
+	@Test
+	public void testApp() {
+		assertTrue(true);
+	}
+
+}
