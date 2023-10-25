9b2715d1b85c3e857b34235784461ada689c1c6a
fabian57fabian <fabiangreavu@gmail.com>
2023-10-25 20:37:44 +0200

    Added parameters in main with picocli

--- iotsystem/src/main/java/com/fabian57fabian/app/swing/IotSwingApp.java
+++ iotsystem/src/main/java/com/fabian57fabian/app/swing/IotSwingApp.java
@@ -10,16 +10,28 @@ import com.fabian57fabian.app.view.IotSwingView;
 import com.mongodb.MongoClient;
 import com.mongodb.ServerAddress;
 
-public class IotSwingApp {
+import java.util.concurrent.Callable;
+import picocli.CommandLine;
+import picocli.CommandLine.Command;
+import picocli.CommandLine.Option;
+
+@Command(mixinStandardHelpOptions = true)
+public class IotSwingApp implements Callable<Void> {
+	
+	@Option(names = { "--mongo-host" }, description = "MongoDB host address")
+	private String mongoHost = "localhost";
+	
+	@Option(names = { "--mongo-port" }, description = "MongoDB host port")
+	private int mongoPort = 27017;
+	
 	public static void main(String[] args) {
+		new CommandLine(new IotSwingApp()).execute(args);
+	}
+	
+	@Override
+	public Void call() throws Exception {
 		EventQueue.invokeLater(() -> {
 			try {
-				String mongoHost = "localhost";
-				int mongoPort = 27017;
-				if (args.length > 0)
-					mongoHost = args[0];
-				if (args.length > 1)
-					mongoPort = Integer.parseInt(args[1]);
 				SystemMongoRepository systemRepository = new SystemMongoRepository(
 						new MongoClient(new ServerAddress(mongoHost, mongoPort)));
 				IotSwingView systemView = new IotSwingView();
@@ -31,5 +43,6 @@ public class IotSwingApp {
 				Logger.getLogger(IotSwingApp.class.getName()).log(Level.SEVERE, "Exception", e);
 			}
 		});
+		return null;
 	}
 }
